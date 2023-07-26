aedbc4b70e395f0fe13c52ad1bcc82c57e713eb0
Carlos de Paula <me@carlosedp.com>
2023-07-26 18:37:27 -0300

    Cleanup

--- httpserver/src/scala/Server.scala
+++ httpserver/src/scala/Server.scala
@@ -1,60 +1,34 @@
 import zio.*
 import zio.http.*
 import zio.http.netty.NettyConfig
-import zio.http.netty.NettyConfig.LeakDetectionLevel
-import zio.http.internal.middlewares.Cors.CorsConfig
-import zio.http.Header.{AccessControlAllowMethods, AccessControlAllowOrigin, Origin}
 
 object ZioHttpApp extends ZIOAppDefault:
   val PORT = 8080
 
-  // Create CORS configuration
-  val corsConfig: CorsConfig =
-    CorsConfig(
-      allowedOrigin = {
-        case origin @ Origin.Value(_, host, _) if host == "dev" =>
-          Some(AccessControlAllowOrigin.Specific(origin))
-        case _ => None
-      },
-      allowedMethods = AccessControlAllowMethods(
-        Method.PUT,
-        Method.DELETE,
-        Method.POST,
-        Method.GET,
-      ),
-    )
   // Add route managers and middleware
   val httpProg =
     (HomeApp() ++ GreetingApp())
-      @@ HttpAppMiddleware.cors(corsConfig)
       @@ HttpAppMiddleware.debug
 
   // Server config
   // ZIO-HTTP server config
-  val configLayer =
-    ZLayer.succeed(
-      Server.Config.default
-        .port(PORT)
-    )
-
-  val nettyConfigLayer = ZLayer.succeed(
-    NettyConfig.default
-      .leakDetection(LeakDetectionLevel.PARANOID)
-      .maxThreads(2)
-  )
+  val configLayer = ZLayer.succeed(Server.Config.default.port(PORT))
 
   // Define ZIO-http server
   val server: ZIO[Any, Throwable, Nothing] = Server
     .serve(httpProg)
     .provide( // Add required layers
       configLayer,
-      nettyConfigLayer,
+      ZLayer.succeed(NettyConfig.default),
       Server.customized,
     )
 
   val console = Console.printLine(s"Server started on http://localhost:${PORT}")
-  def run     = console *> server
 
+  // Run main application
+  def run = console *> server
+
+// These are our route managers
 object HomeApp:
   def apply(): Http[Any, Nothing, Request, Response] =
     Http.collectZIO[Request] { case Method.GET -> Root =>
